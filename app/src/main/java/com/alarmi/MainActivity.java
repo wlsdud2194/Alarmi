4813d954657ef25b71bed172c8038483d4910743
wlsdud2194 <38432821+wlsdud2194@users.noreply.github.com>
2020-06-04 14:22:00 +0900

    project init

---
+++ app/src/main/java/com/alarmi/MainActivity.java
@@ -0,0 +1,18 @@
+package com.alarmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alarmi.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
